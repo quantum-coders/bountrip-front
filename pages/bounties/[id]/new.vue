0a9680017ba9d03f7bd0560171d3ba2a7bceed56
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-10-29 21:19:50 -0600

    ✨ Added change-case library for string manipulation
    The `change-case` library was added to the project dependencies in `package.json`. This library provides functions for manipulating strings, which will be useful for capitalizing the first letter of a place category.
    
    🔍 Improved search functionality in new.vue
    
    In the `new.vue` file, the search functionality for places has been improved. The search input field is now wrapped in a div with a search icon. Additionally, when a place is added, the value of the input field is cleared after a short delay.
    
    🌍 Added Google Maps integration
    
    A Google Map has been integrated into the page using the `google.maps.Map` constructor. The map is centered at latitude 19.487037542176687 and longitude -99.26139595311078 with a zoom level of 15.
    
    💄 Updated styling
    
    Several style changes have been made to improve the layout and appearance of elements on the page:
    - The search container now has a box shadow and rounded corners.
    - Place categories are displayed as badges on top-right corner of each place.
    - A textarea has been added for adding comments to each place.
    - The sidebar now has padding at its bottom.
    - The map container now has a fixed height and rounded corners.
    
    Note: There was no newline at end of file in this commit.

--- pages/bounties/[id]/new.vue
+++ pages/bounties/[id]/new.vue
@@ -15,31 +15,45 @@
 					<h4>Places and Recommendations</h4>
 					<p>Search for places to recommend them for your plan</p>
 
-					<project-places-search
-						class="form-control"
-						placeholder="Search for a place"
-						@place-changed="addPlace"
-					/>
+					<div class="search">
+						<project-places-search
+							placeholder="Search for a place"
+							@place-changed="addPlace"
+						/>
+						<icon name="material-symbols:search-rounded" />
+					</div>
 
 					<template v-for="p in places">
 						<article class="place">
-							
 
-							<img class="place-thumb" :src="p.photos[0].url" alt="">
+							<img v-if="p.photos" class="place-thumb" :src="p.photos[0].url" alt="">
+							<div v-else class="place-thumb" />
 
 							<div class="place-info">
+								<div class="place-category">{{ changeCase.capitalCase(p.types[0]) }}</div>
 
 								<h4 class="place-title mb-0">{{ p.name }}</h4>
-								<p class="d-flex align-items-center gap-3 mb-0">
-									<plan-rating :rating="p.rating" />
-									<span class="price-level">
-										<icon name="material-symbols:attach-money" v-for="i in p.price_level" />
+								<p class="d-flex align-items-center gap-2 mb-0">
+									<span v-if="p.rating">
+										<plan-rating :rating="p.rating" />
 									</span>
+									<span class="reviews" v-if="p.reviews">
+										{{ p.reviews.length }} review{{ p.reviews.length > 1 ? 's' : '' }}
+									</span>
+									<!--<span class="price-level">
+										<icon name="material-symbols:attach-money" v-for="i in p.price_level" />
+									</span>-->
 								</p>
 								<p>{{ p.formatted_address }}</p>
 
-								<p>
-								</p>
+								<!-- textarea for comments -->
+								<div class="form-group">
+									<textarea
+										class="form-control"
+										rows="3"
+										placeholder="Add a comment for this place"
+									/>
+								</div>
 							</div>
 						</article>
 					</template>
@@ -58,6 +72,8 @@
 						></textarea>
 					</div>
 
+					<div class="map" id="map" />
+
 				</aside>
 			</div>
 		</div>
@@ -65,13 +81,32 @@
 </template>
 
 <script setup>
+	import * as changeCase from 'change-case';
+
 	definePageMeta({ layout: 'bountrip' });
 
 	const places = ref([]);
 
 	const addPlace = (place) => {
 		places.value.push(place);
+
+		// remove value to #places
+		setTimeout(() => {
+			document.querySelector('.search input').value = '';
+		}, 10);
 	};
+
+	onMounted(() => {
+
+		// setup a google map
+		const map = new google.maps.Map(document.getElementById('map'), {
+			center: {
+				lat: 19.487037542176687,
+				lng: -99.26139595311078
+			},
+			zoom: 15,
+		});
+	});
 </script>
 
 <!--suppress SassScssResolvedByNameOnly -->
@@ -84,11 +119,33 @@
 			padding: 0 1rem
 			flex-grow: 1
 
+			.search
+				display: flex
+				align-items: center
+				gap: 1rem
+				margin-bottom: 3rem
+				box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1)
+				border-radius: 0.5rem
+				padding: 1rem
+
+				input
+					flex-grow: 1
+					border: 0
+
+					&:focus
+						outline: none
+
 		.plan-sidebar
 			width: 400px
 			flex-shrink: 0
 			border-left: 1px solid var(--bs-border-color)
-			padding: 0 1rem
+			padding: 0 1rem 1rem
+
+			.map
+				width: 100%
+				height: 600px
+				border-radius: 0.5rem
+				margin-top: 1rem
 
 		.place
 			display: flex
@@ -101,11 +158,25 @@
 				width: 100px
 				aspect-ratio: 1
 				border-radius: 0.5rem
+				background: #EEE
 
 			.place-info
 				flex-grow: 1
 
+				.place-category
+					font-size: 0.75rem
+					position: absolute
+					right: 0
+					top: 0
+					background: var(--brand2)
+					color: white
+					padding: 0.25rem 0.5rem
+					border-radius: 10rem
+
 				.place-title
 					font-size: 1.2rem
+
+				textarea
+					resize: none
 
 </style>
