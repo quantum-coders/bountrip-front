ab9ff2355d5b61fc0635711a37aafc3d90a9a278
Rodrigo Tejero <rodrigo.tejero@chimp.mx>
2024-10-30 02:47:31 -0600

    🔄 Updated ecosystem.config.js
    This commit updates the configuration file `ecosystem.config.js`. The changes include:
    - Added a new app configuration for 'bountrip-front'
    - Set the port to '1555'
    - Configured the execution mode as 'cluster'
    - Set the number of instances to 1
    - Updated the script path to './.output/server/index.mjs'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  apps: [
-    {
-      name: 'bountrip-front',
-      port: '1555',
-      exec_mode: 'cluster',
-      instances: 1,
-      script: './.output/server/index.mjs'
-    }
-  ]
-}
+	apps: [
+		{
+			name: 'bountrip-front',
+			port: '1555',
+			exec_mode: 'cluster',
+			instances: 1,
+			script: './.output/server/index.mjs',
+		},
+	],
+};
